bf00f05c3614b5e9db5e6167cd5bd28ebe6d6dd9
Emre Derman <37815224+emredermann@users.noreply.github.com>
2022-03-09 00:49:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    # TODO: Write a globally unique name which will be listed on PyPI
-    name="very-simple-dictionary",
-    author="Emre Sülün",  # TODO: Write your name
+
+    name="simple-homework-CS453_123-dictionary",
+    author="Emre Derman",  
     version="2.0.0",
     packages=["dictionary"],
     install_requires=[
